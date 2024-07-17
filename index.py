bce850f6d16ae063ab3208d98dae64a3171ca08d
leoneric0212 <leoneric0212@gmail.com>
2024-07-17 16:57:18 +0800

    modify

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
-import platform
+import platform     #配合不同作業系統的顯示字體
 import matplotlib.pyplot as plt  # Import Matplotlib
 from tkintermapview import TkinterMapView
 import pandas as pd
@@ -13,7 +13,6 @@ class Window(tk.Tk):
         self.geometry("1280x900")  
 
         self.init_vars()
-
         self.setup_gui()
         
     def init_vars(self):
@@ -21,21 +20,21 @@ class Window(tk.Tk):
         self.months = list(range(1, 13))
         self.days = list(range(1, 32))
 
-        # Initialize variables for city selection
+        # 設定縣市資料的資料表
         self.cities = ["臺北市", "新北市", "基隆市", "桃園市", "新竹市", "新竹縣", "苗栗縣", "臺中市", "臺中縣", "彰化縣", "南投縣", "雲林縣", "嘉義市", "嘉義縣", "臺南市", "高雄市", "宜蘭縣", "花蓮縣", "臺東縣", "澎湖縣", "金門縣", "連江縣"]
 
-        # Initialize variables for weather and light conditions
+        # 設定天氣與光線的資料表
         self.weathers = ["晴", "陰", "雨"]
         self.lights = ["日間自然光線", "夜間(或隧道)有照明", "有照明且開啟", "無照明", "晨或暮光", "夜間(或隧道)無照明", "有照明未開啟或故障"]
 
-        # Initialize variables for checkboxes
+        # 設定過濾器的變數值
         self.city_vars = {city: tk.BooleanVar() for city in self.cities}
         self.weather_vars = {weather: tk.BooleanVar(value=True) for weather in self.weathers}
         self.light_vars = {light: tk.BooleanVar(value=True) for light in self.lights}
         self.run_vars = {"是": tk.BooleanVar(value=True), "否": tk.BooleanVar(value=True)}
 
     def setup_gui(self):
-
+        #主頁面設定
         mainframe = ttk.Frame(self)
         mainframe.pack(expand=True, fill='both', padx=10, pady=10)
 
@@ -53,11 +52,11 @@ class Window(tk.Tk):
         extra_frame = ttk.Labelframe(left_top_frame, text="進階選項：")
         extra_frame.grid(column=0, row=2, padx=10, pady=10, sticky=tk.W)
         self.setup_extra_widgets(extra_frame)
-        
+
+        #設定送出篩選按鈕及結果數目顯示
         self.submit_frame = ttk.Labelframe(left_top_frame, text="篩選結果：")
         self.submit_frame.grid(column=0, row=3, padx=10, pady=10, sticky=tk.W)
         ttk.Label(self.submit_frame, text=f"案件數目：").grid(column=0, row=0, padx=10, pady=10, sticky=tk.W)
-        
         submit_button=ttk.Button(left_top_frame, text="送出", command=self.submit_data).grid(column=0, row=4, padx=10, pady=10, sticky=tk.E)
         
         right_top_frame = ttk.Labelframe(mainframe, text="事故地圖")
@@ -81,7 +80,7 @@ class Window(tk.Tk):
         self.day.set(self.days[0])
         self.day.grid(column=4, row=0, padx=5, pady=5)
         ttk.Label(parent, text="日").grid(column=5, row=0, padx=5, pady=5, sticky=tk.E)
-
+        #設定日期按鈕自動變動
         self.year.bind("<<ComboboxSelected>>", self.update_dates)
         self.month.bind("<<ComboboxSelected>>", self.update_dates)
         self.day.bind("<<ComboboxSelected>>", self.update_dates)
@@ -111,7 +110,6 @@ class Window(tk.Tk):
         self.map = TkinterMapView(parent, width=600, height=400)
         self.map.grid(column=0, row=0, padx=10, pady=10)
         self.map.set_position(25.115045154785246, 121.53834693952264,marker=True)
-        
 
         self.pie_chart_button = ttk.Button(parent, text="顯示对应的线图", command=self.show_charts)
         self.pie_chart_button.grid(column=0, row=1, padx=10, pady=10)
@@ -119,12 +117,12 @@ class Window(tk.Tk):
     def setup_treeview(self, parent):
         self.treeview = ttk.Treeview(parent, columns=('#0','#1', '#2', '#3', '#4', '#5', '#6', '#7', '#8','#9'),show='headings')
         self.treeview.grid(column=0, row=0, sticky='nsew')
-
+        #設定下方資料表單
         headings = ['發生日期', '發生時間', '事故類別', '發生地點', '天氣', '光線名稱','速限', '道路類別','事故類型','死亡受傷人數']
         for i, col in enumerate(headings,start=1):
             self.treeview.heading('#' + str(i), text=col, anchor='center')
             self.treeview.column('#' + str(i), minwidth=60, width=120, anchor='s')
-
+        #設定資料表單滾動軸
         scrollbar = ttk.Scrollbar(parent, orient="vertical", command=self.treeview.yview)
         scrollbar.grid(column=1, row=0, sticky='ns')
         self.treeview.configure(yscrollcommand=scrollbar.set)
@@ -137,7 +135,7 @@ class Window(tk.Tk):
         selected_cities = [city for city, var in self.city_vars.items() if var.get()]
         selected_weathers = [weather for weather, var in self.weather_vars.items() if var.get()]
         selected_lights = [light for light, var in self.light_vars.items() if var.get()]
-        
+        #讀取檔案
         try:
             df=pd.read_csv(f"./data/{selected_year}.csv",encoding='utf-16')
             df.columns=df.columns.str.strip()
@@ -146,7 +144,7 @@ class Window(tk.Tk):
         df['發生日期']=pd.to_datetime(df['發生日期'])
         df['month']=df['發生日期'].dt.month
         df['day']=df['發生日期'].dt.day
-
+        #利用pandas功能篩選資料
         filtered_df = df[
             (df['month'] == selected_month) &
             (df['day'] == selected_day) &
@@ -157,21 +155,22 @@ class Window(tk.Tk):
         result_count = len(filtered_df)
         self.update_counts(result_count)
         self.populate_respond(filtered_df)
-        
+    
+    #自動資料數目計算刷新功能
     def update_counts(self,count):
-        for widget in self.submit_frame.winfo_children():   #後面這段看不懂
+        for widget in self.submit_frame.winfo_children():   
             widget.destroy()
         ttk.Label(self.submit_frame, text=f"案件數目：{count}").grid(column=1, row=0, padx=10, pady=10, sticky=tk.W)
         
     def populate_respond(self,data):
         for row in self.treeview.get_children():
             self.treeview.delete(row)
-
+        #整理日期與時間資料型態
         for _, row in data.iterrows():
             formatted_time=row['發生時間'].split('.')[0]
             formatted_date=row['發生日期'].strftime('%Y-%m-%d')
 
-        # Read populate Treeview
+        #讀取資料
             self.treeview.insert('','end', values=(
                 formatted_date,
                 formatted_time,
@@ -185,10 +184,9 @@ class Window(tk.Tk):
                 row['死亡受傷人數']
             ))
 
-        # Read populate Map
+        #將讀取資料匯入地圖
         self.map.delete_all_marker()
         for _, row in data.iterrows():
-            
             lat=float(row['緯度'])
             lng=float(row['經度'])
             self.map.set_position(lat,lng,marker=True)
@@ -221,10 +219,10 @@ class Window(tk.Tk):
         return df
     
     def show_charts(self):
-        if platform.system() == 'Windows':
+        if platform.system() == 'Windows':      #Windows
             plt.rcParams['font.sans-serif'] = ['Microsoft YaHei', 'SimHei', 'SimSun']
-        elif platform.system() == 'Darwin':  # macOS
-            plt.rcParams['font.sans-serif'] = ['PingFang', 'STHeiti', 'Arial Unicode MS']# 使用中文字體
+        elif platform.system() == 'Darwin':   #macOS
+            plt.rcParams['font.sans-serif'] = ['PingFang', 'STHeiti', 'Arial Unicode MS'] # 使用中文字體
         df = self.get_treeview_data()
         if df.empty:
             messagebox.showerror("錯誤","未選擇資料")
